63844b54f8ee0027698cefff0645f21409e65bd0
Juan J. Torres <juanjtorres@pm.me>
2019-10-25 00:00:53 -0500

    constantes algoritmos

--- src/cliente/Client.java
+++ src/cliente/Client.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package cliente;
 
 /**
@@ -10,13 +7,43 @@ package cliente;
 public class Client
 {
 
+	// CADENAS DE CONTROL
+
+	public static final String HOLA = "HOLA";
+
+	public static final String OK = "OK";
+
+	public static final String ERROR = "ERROR";
+
+	// SEPARADOR
+
+	public static final String SEPARADOR = " : ";
+
+	// ALGORITMOS SIMÉTRICOS
+
+	public static final String AES = "AES";
+
+	public static final String BLOWFISH= "BLOWFISH";
+
+	// ALGORITMOS ASIMÉTRICOS
+
+	public static final String RSA = "RSA";
+
+	// ALGORITMOS HMAC
+
+	public static final String SHA1 = "HMACSHA1";
+
+	public static final String SHA256 = "HMACSHA256";
+
+	public static final String SHA384 = "HMACSHA384";
+
+	public static final String SHA512 = "HMACSHA512";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 }
