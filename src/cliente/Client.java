41dd476023acc0b72ef30edceec9fcb871fca077
Juan J. Torres <juanjtorres@pm.me>
2019-10-25 16:52:12 -0500

    Etapa 1 del protocolo finalizada

--- src/cliente/Client.java
+++ src/cliente/Client.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -12,7 +11,6 @@ import java.net.Socket;
  */
 public class Client
 {
-
     // CADENAS DE CONTROL
 
     public static final String HOLA = "HOLA";
@@ -21,9 +19,11 @@ public class Client
 
     public static final String ERROR = "ERROR";
 
+    public static final String ALGORITMOS = "ALGORITMOS";
+
     // SEPARADOR
 
-    public static final String SEPARADOR = " : ";
+    public static final String SEPARADOR = ":";
 
     // ALGORITMOS SIMÉTRICOS
 
@@ -49,19 +49,22 @@ public class Client
 
     private Socket canal;
 
-    private BufferedReader in;
+    private BufferedReader inServidor;
+
+    private BufferedReader inUsuario;
 
     private PrintWriter out;
 
     // CONSTRUCTOR
 
-    public Client(int pPuerto)
+    public Client(BufferedReader in, int pPuerto)
     {
         try
         {
-            canal = new Socket("localhost", pPuerto);
-            in = new BufferedReader(new InputStreamReader(canal.getInputStream()));
-            out = new PrintWriter(canal.getOutputStream(), true);
+            setCanal(new Socket("localhost", pPuerto));
+            setInServidor(new BufferedReader(new InputStreamReader(getCanal().getInputStream())));
+            setInUsuario(in);
+            setOut(new PrintWriter(getCanal().getOutputStream(), true));
         }
         catch (IOException e)
         {
@@ -72,9 +75,6 @@ public class Client
 
     // MÉTODOS
 
-    /**
-     * @param args
-     */
     public static void main(String[] args)
     {
         try
@@ -82,21 +82,97 @@ public class Client
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Ingrese el puerto por el que se desea conectar");
             int puerto = Integer.parseInt(input.readLine());
-            Client cliente = new Client(puerto);
-            cliente.comunicacion();
+            Client cliente = new Client(input, puerto);
+            cliente.comunicacion1();
+            String certificado = cliente.getInServidor().readLine();
+            cliente.comunicacion2(certificado);
+            cliente.comunicacion3();
+            cliente.comunicacion4();
         }
-        catch (IOException e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
     }
 
-    public void comunicacion()
+    public Socket getCanal()
+    {
+        return canal;
+    }
+
+    public void setCanal(Socket canal)
+    {
+        this.canal = canal;
+    }
+
+    public BufferedReader getInServidor()
+    {
+        return inServidor;
+    }
+
+    public void setInServidor(BufferedReader inServidor)
+    {
+        this.inServidor = inServidor;
+    }
+
+    public BufferedReader getInUsuario()
+    {
+        return inUsuario;
+    }
+
+    public void setInUsuario(BufferedReader inUsuario)
+    {
+        this.inUsuario = inUsuario;
+    }
+
+    public PrintWriter getOut()
+    {
+        return out;
+    }
+
+    public void setOut(PrintWriter out)
+    {
+        this.out = out;
+    }
+
+    /**
+     * Etapa 1 de la comunicación con el servidor en que se seleccionan los algoritmos a usar.
+     *
+     * @throws Exception En caso de que el servidor mande un mensaje de ERROR.
+     */
+    public void comunicacion1() throws Exception
     {
         try
         {
-            out.println("HOLA");
-            System.out.println(in.readLine());
+            getOut().println(HOLA);
+            if (ERROR.equals(getInServidor().readLine()))
+                throw new Exception("Hubo un error al iniciar la comunicación con el servidor");
+            System.out.println();
+            System.out.println("Seleccione el algoritmo simetrico que quiere :usar (1,2) \n" +
+                                       "1) AES\n" +
+                                       "2) Blowfish");
+            int res1 = Integer.parseInt(getInUsuario().readLine());
+            String simetrico = "";
+            simetrico = res1 == 1 ? AES : BLOWFISH;
+            System.out.println("Seleccione el algoritmo HMAC que quiere :usar (1,2,3,4) \n" +
+                                       "1) HmacSHA1\n" +
+                                       "2) HmacSHA256\n" +
+                                       "3) HmacSHA384\n" +
+                                       "4) HmacSHA512");
+            int res2 = Integer.parseInt(getInUsuario().readLine());
+            String hmac = "";
+            if (1 == res2)
+                hmac = SHA1;
+            else if (2 == res2)
+                hmac = SHA256;
+            else if (3 == res2)
+                hmac = SHA384;
+            else
+                hmac = SHA512;
+            System.out.println(ALGORITMOS + SEPARADOR + simetrico + SEPARADOR + RSA + SEPARADOR + hmac);
+            getOut().println(ALGORITMOS + SEPARADOR + simetrico + SEPARADOR + RSA + SEPARADOR + hmac);
+            if (ERROR.equals(getInServidor().readLine()))
+                throw new Exception("Hubo un error al enviar algoritmos de cifrado al servidor");
         }
         catch (IOException e)
         {
@@ -104,4 +180,27 @@ public class Client
         }
     }
 
+    /**
+     * Etapa 2 del protocolo de comunicación, se autenticará al servidor.
+     *
+     * @param certificado certificado del servidor web
+     */
+    public void comunicacion2(String certificado)
+    {
+        System.out.println(certificado);
+    }
+
+    /**
+     * Etapa 3 del protocolo de comunicación, se autenticará al cliente.
+     */
+    public void comunicacion3()
+    {
+    }
+
+    /**
+     * Etapa 4 del protooolo de comunicación, se solicita información y se valifa la respuesta.
+     */
+    public void comunicacion4()
+    {
+    }
 }
